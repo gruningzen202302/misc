5dd420612e485e72f49b4a3f688b4bcefb1729ca
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-12-08 23:04:15 -0300

    Add FiniteStateMachine class to map order states

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,80 @@
 console.log("Leet Code in JS");
 
 
+class FiniteStateMachine {
+  constructor() {
+    this.states = {
+      currentState: null, //"Pending", //null , //q0
+      finalState: "Complete", //q1
+      inicialState: "Pending", //q0
+    };
+    this.transition = {
+      delta: (input, state = this.states.inicialState) => {
+        console.log("input from delta", input);
+        if (input.length === 0) return;
+        //let currentInput = input.shift()//TODO implement more efficient approach if it is easy to convert to othjer languages
+        let currentInput = input[0];
+        input = input.slice(1); //, input.length)
+        let mapping = this.transition.mapping(state);
+        this.states.currentState = mapping.get(currentInput);
+        this.transition.delta(input, this.states.currentState);
+      },
+      mapping: (state) => {
+        let stateMap = new Map();
+        stateMap.set(
+          "Pending",
+          new Map([
+            ["0", "Pending"],
+            ["1", "On Hold"],
+            ["2", "Processing"],
+            ["3", "Cancelled"],
+          ])
+        );
+        stateMap.set(
+          "On Hold",
+          new Map([
+            ["0", "On Hold"],
+            ["1", "Processing"],
+            ["2", "Cancelled"],
+            ["3", "Complete"],
+            ["4", "Closed"],
+            ["5", "Payment Review"],
+            ["6", "Pending Payment"],
+          ])
+        );
+        stateMap.set(
+          "Processing",
+          new Map([
+            ["0", "On Hold"],
+            ["1", "Cancelled"],
+            ["2", "Complete"],
+            ["4", "Payment Review"],
+            ["5", "Pending Payment"],
+          ])
+        );
+        stateMap.set("Complete", new Map([["0", "Complete"]]));
 
+        return stateMap.get(state);
+      },
+    };
+  }
+
+  evaluate(input) {
+    console.log("input", input);
+    this.transition.delta(input);
+    let result;
+    this.states.currentState === this.states.finalState
+      ? (result = true)
+      : (result = false);
+    return result;
+  }
+}
+
+let shopOrderStateMachine = new FiniteStateMachine();
+let input = ["Complete"]; //["Pending", "Processing", "Complete"];
+let result = shopOrderStateMachine.evaluate(input);
+//console.warn("shopOrderStateMachine")
+console.log(result);
 
 
 
